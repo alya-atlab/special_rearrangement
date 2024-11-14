7cb2f6bef47bb9630ddcb4e10eae5386bf3c8a69
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-14 16:00:24 +0200

    Add comments

--- special_rearrangement.py
+++ special_rearrangement.py
@@ -1,11 +1,15 @@
 def special_rearrangement(nums):
-    new_list = []
+    new_list = []  # create a new list to rearrange the old list
     for i in nums:
-        if i % 2 == 0:
+        if (
+            i % 2 == 0
+        ):  # if the item is even add it to the new list and remove it from the old one
             new_list.append(i)
             nums.remove(i)
 
-    return new_list + nums
+    return (
+        new_list + nums
+    )  # after that return the new list + the old list so the even numbers will came before the odd
 
 
 print(special_rearrangement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
